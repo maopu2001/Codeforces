7af01ec4f27b1d7c0f8faa043026fdc5c0cd9db5
M. Aktaruzzaman Opu <maopu2001@gmail.com>
2022-11-01 19:05:00 +0600

    01/11/2022

--- A_Football.cpp
+++ A_Football.cpp
@@ -6,23 +6,20 @@ using ll = long long;
 using u32 = unsigned int;
 using u64 = unsigned long long;
 
-#define pr pair<int, int>
 #define pb push_back
-#define mp make_pair
 #define p1 first
 #define p2 second
+#define pr pair<int, int>
 #define vc vector<int>
-#define vvc vector<vc>
+#define si set<int>
+#define mpi map<int, int>
 
 #define nl endl
-#define yes cout << "YES" << nl
-#define no cout << "NO" << nl
 #define FOR(i, a) for (int i = 0; i < int(a); ++i)
 #define FOR3(i, a, b) for (int i = a; i <= int(b); ++i)
-#define FOR4(i, a, b, c) for (int i = a; i <= int(b); i += (c))
 #define FOR_R(i, a) for (int i = (a)-1; i >= int(0); --i)
-#define FOR3_R(i, a, b) for (int i = (b)-1; i >= int(a); --i)
-#define FOR4_R(i, a, b, c) for (int i = (b)-1; i >= int(a); i -= (c))
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
 
 #define all(x) x.begin(), x.end()
 #define len(x) int(x.size())
@@ -33,51 +30,42 @@ using u64 = unsigned long long;
 #define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
 #define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
 #define UNIQUE(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+#define SUM(v) accumulate(all(v), 0)
 
-int SUM(vc &A)
-{
-  int sum = 0;
-  for (auto a : A)
-    sum += a;
-  return sum;
-}
-void print(vc &A)
-{
-  for (auto i : A)
-    cout << i << " ";
+#define CIN(v)                                                                 \
+  for (auto &it : v)                                                           \
+    cin >> it;
+#define COUT(v)                                                                \
+  for (auto &it : v)                                                           \
+    cout << it << " ";                                                         \
   cout << nl;
-}
 
-void solve()
-{
-  string s;
-  cin >> s;
-  int n = s.size();
-
-  FOR(i, n - 6)
-  {
-    int k = i, one = 0, zero = 0;
-    for (int j = 0, k = i; j < 7; j++, k++)
-    {
-      (s[k] == '1') ? one++ : zero++;
-      if (one == 7 || zero == 7)
-      {
-        yes;
-        return;
-      }
-    }
+void solve() {
+  int n, max = 0;
+  cin >> n;
+  unordered_map<string, int> m;
+  FOR(i, n) {
+    string s;
+    cin >> s;
+    if (m.find(s) == m.end())
+      m.insert({s, 1});
+    else
+      m[s]++;
   }
-
-  no;
+  pair<string, int> p = {"\0", 0};
+  for (auto i = m.begin(); i != m.end(); i++) {
+    if (i->p2 > p.p2)
+      p = {i->p1, i->p2};
+  }
+  cout << p.p1 << nl;
 }
 
-int32_t main()
-{
+int32_t main() {
   // cout << setprecision(2) << fixed;
 
-  // int T;
-  // cin >> T;
-  // FOR(_, T)
+  // int _;
+  // cin >> _;
+  // while (_--)
   solve();
   return 0;
 }
