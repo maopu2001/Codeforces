bc490c63ef70b854fee8bbfb0c043b03b3ab521a
M. Aktaruzzaman Opu <maopu2001@gmail.com>
2022-11-06 22:06:36 +0600

    06/11/2022

--- A_Everyone_Loves_to_Sleep.cpp
+++ A_Everyone_Loves_to_Sleep.cpp
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
   int t;
   cin >> t;
-  while (t--)
-  {
-    int n, h, m, ah, am;
-    set<int> alarm;
+  while (t--) {
+    int n, h, m, ah, am, sleep, ans = 24 * 60;
     cin >> n >> h >> m;
+    sleep = h * 60 + m;
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
       int ah, am;
       cin >> ah >> am;
-      alarm.insert(ah);
-      alarm.insert(am);
+      int cal_ans = (ah * 60 + am) - sleep;
+      if (cal_ans < 0)
+        cal_ans += 24 * 60;
+      ans = min(ans, cal_ans);
     }
+    cout << ans / 60 << " " << ans % 60 << endl;
   }
 }
