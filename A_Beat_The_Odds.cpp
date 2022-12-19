d58c3010fcd67e1a893265fbed3d7deca3861adf
M. Aktaruzzaman Opu <maopu2001@gmail.com>
2022-12-19 20:42:04 +0600

    19/12/2022

---
+++ A_Beat_The_Odds.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl endl
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  int n, e = 0;
+  cin >> n;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++) {
+    cin >> v[i];
+    if (v[i] % 2 == 0)
+      e++;
+  }
+  cout << min(e, n - e) << nl;
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, T;
+  cin >> _;
+  for (int i = 1; i <= _; ++i)
+    solve(T);
+  return 0;
+}
---
+++ A_Min_Max_Swap.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl endl
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  int n;
+  cin >> n;
+  vector<int> a(n), b(n);
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+  for (int i = 0; i < n; i++)
+    cin >> b[i];
+
+  for (int i = 0; i < n; i++)
+    if (a[i] > b[i])
+      swap(a[i], b[i]);
+
+  cout << *max_element(a.begin(), a.end()) * *max_element(b.begin(), b.end())
+       << nl;
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, T;
+  cin >> _;
+  for (int i = 1; i <= _; ++i)
+    solve(T);
+  return 0;
+}
