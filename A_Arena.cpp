c5c353d70f68fe02e36d89db838a379ad5cae311
M. Aktaruzzaman Opu <maopu2001@gmail.com>
2022-12-28 21:10:18 +0600

    28/12/2022

---
+++ A_Arena.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl endl
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  int n, m = INT_MAX;
+  cin >> n;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++) {
+    cin >> v[i];
+    m = min(m, v[i]);
+  }
+  int ans = n;
+  for (int i = 0; i < n; i++)
+    if (v[i] == m)
+      ans--;
+
+  cout << ans << nl;
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, T;
+  cin >> _;
+  for (int i = 1; i <= _; ++i)
+    solve(T);
+  return 0;
+}
