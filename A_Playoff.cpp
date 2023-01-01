852588d1f9b5fd765772f951d04b6d526d22393a
M. Aktaruzzaman Opu <maopu2001@gmail.com>
2023-01-01 19:56:02 +0600

    01/01/2023

---
+++ A_Playoff.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl endl
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  int n;
+  cin >> n;
+  cout << int(pow(2, n) - 1) << nl;
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, T;
+  cin >> _;
+  for (int i = 1; i <= _; ++i)
+    solve(T);
+  return 0;
+}
