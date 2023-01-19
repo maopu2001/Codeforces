ef88533244b6a3b28726d86b7965e539c92b7a04
M. Aktaruzzaman Opu <maopu2001@gmail.com>
2023-01-19 19:46:52 +0600

    19/01/2023

---
+++ A_Two_Rabbits.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl endl
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  int x, y, a, b;
+  cin >> x >> y >> a >> b;
+  if ((y - x) % (b + a) == 0)
+    cout << (y - x) / (b + a) << nl;
+  else
+    cout << -1 << nl;
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, T;
+  cin >> _;
+  for (int i = 1; i <= _; ++i)
+    solve(T);
+  return 0;
+}
