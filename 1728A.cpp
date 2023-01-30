f35a7bc242106a915dde3ceaf5cadfc2dff4aedb
M. Aktaruzzaman Opu <maopu2001@gmail.com>
2023-01-30 19:50:55 +0600

    30/01/2023

---
+++ 1728A.cpp
@@ -0,0 +1,29 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl endl
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++) {
+    cin >> v[i];
+  }
+  cout << max_element(v.begin(), v.end()) - v.begin() + 1 << nl;
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, T;
+  cin >> _;
+  for (int i = 1; i <= _; ++i)
+    solve(T);
+  return 0;
+}
