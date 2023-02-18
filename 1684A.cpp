1192115d65f7ee19b8bff3b14941140ffb408e90
M. Aktaruzzaman Opu <maopu2001@gmail.com>
2023-02-18 22:04:16 +0600

    18/02/2023

---
+++ 1684A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl endl
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  string s;
+  cin >> s;
+  if (s.size() == 2)
+    cout << s[1] << nl;
+  else
+    cout << *min_element(s.begin(), s.end()) << nl;
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, i;
+  cin >> _;
+  for (i = 1; i <= _; ++i)
+    solve(i);
+  return 0;
+}
