c19b7e1d0bacbbf662d2fb82da711b5340d08e18
M. Aktaruzzaman Opu <maopu2001@gmail.com>
2023-03-23 23:36:22 +0600

    23/03/2023

---
+++ 1807A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl endl
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  int x, y, z;
+  cin >> x >> y >> z;
+  if (z == x + y)
+    cout << "+" << nl;
+  else
+    cout << "-" << nl;
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, i;
+  cin >> _;
+  for (i = 1; i <= _; ++i)
+    solve(i);
+  return 0;
+}
