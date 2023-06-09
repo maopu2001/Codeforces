bbc1d3629617cfe5a2d87fa8ecca56c50afee821
maopu2001 <maopu2001@gmail.com>
2023-06-09 22:48:54 +0600

    09/06/2023

---
+++ 1833C.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl endl
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int &x : a)
+    cin >> x;
+  sort(a.begin(), a.end());
+
+  if (a[0] % 2 == 1) {
+    cout << "YES" << nl;
+    return;
+  }
+  for (int &x : a) {
+    if (x % 2 == 1) {
+      cout << "NO" << nl;
+      return;
+    }
+  }
+  cout << "YES" << nl;
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, i;
+  cin >> _;
+  for (i = 1; i <= _; ++i)
+    solve(i);
+  return 0;
+}
