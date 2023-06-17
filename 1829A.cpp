7a0c79cdf6fe799c6b02e5823ece546cbf0e2bde
maopu2001 <maopu2001@gmail.com>
2023-06-17 19:57:01 +0600

    17/06/2023

---
+++ 1829A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl endl
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  string s, check = "codeforces";
+  cin >> s;
+  int ans = 0;
+  for (int i = 0; i < 10; i++) {
+    if (s[i] != check[i]) {
+      ans++;
+    }
+  }
+  cout << ans << nl;
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, i;
+  cin >> _;
+  for (i = 1; i <= _; ++i)
+    solve(i);
+  return 0;
+}
