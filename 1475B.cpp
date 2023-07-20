74c026f395c4debcab804b37a399917b047933ef
maopu2001 <maopu2001@gmail.com>
2023-07-20 23:59:55 +0600

    20/07/2023

---
+++ 1475B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl endl
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  int n;
+  cin >> n;
+  int cnt2021 = n % 2020;
+  int cnt2020 = (n - cnt2021) / 2020 - cnt2021;
+  if (cnt2020 >= 0 && 2020 * cnt2020 + 2021 * cnt2021 == n)
+    cout << "YES" << nl;
+  else
+    cout << "NO" << nl;
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, i;
+  cin >> _;
+  for (i = 1; i <= _; ++i)
+    solve(i);
+  return 0;
+}
