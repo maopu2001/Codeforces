cd06eeee79870a085fb0f8b1f86e0be55364a063
maopu2001 <maopu2001@gmail.com>
2023-07-22 22:07:45 +0600

    22/07/2023

---
+++ 1343A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl endl
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  int n;
+  cin >> n;
+  for (int i = 2; i < 30; i++) {
+    int val = (1 << i) - 1;
+    if (n % val == 0) {
+      cout << n / val << nl;
+      return;
+    }
+  }
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, i;
+  cin >> _;
+  for (i = 1; i <= _; ++i)
+    solve(i);
+  return 0;
+}
