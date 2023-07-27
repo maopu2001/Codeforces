cd0a8bc8cddfbcd07cf2d69dc55ee828e09a9f8a
maopu2001 <maopu2001@gmail.com>
2023-07-27 21:52:22 +0600

    27/07/2023

---
+++ 1850A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl endl
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  int a[3];
+  cin >> a[0] >> a[1] >> a[2];
+  sort(a, a + 3);
+  cout << (a[1] + a[2] >= 10 ? "YES" : "NO") << nl;
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, i;
+  cin >> _;
+  for (i = 1; i <= _; ++i)
+    solve(i);
+  return 0;
+}
