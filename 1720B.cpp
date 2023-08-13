428a089afbf047c20e61500cd5d6dcece6c98ed2
maopu2001 <maopu2001@gmail.com>
2023-08-13 20:03:55 +0600

    13/08/2023

---
+++ 1720B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  int n;
+  cin >> n;
+  vector<int> a(n + 1);
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+  sort(a.begin(), a.end());
+  cout << a[n] + a[n - 1] - a[1] - a[2] << nl;
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  // ios_base :: sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, i;
+  cin >> _;
+  for (i = 1; i <= _; ++i)
+    solve(i);
+  return 0;
+}
