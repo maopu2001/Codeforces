967bdb6bf0eed124df55fecbb2b88ca2acb25a36
maopu2001 <maopu2001@gmail.com>
2023-09-09 21:30:35 +0600

    09/09/2023

---
+++ 1838A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int &i : a)
+    cin >> i;
+
+  int m = *min_element(a.begin(), a.end());
+  int M = *max_element(a.begin(), a.end());
+
+  cout << (m < 0 ? m : M) << nl;
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  // ios_base :: sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, i;
+  cin >> _;
+  for (i = 1; i <= _; ++i)
+    solve(i);
+  return 0;
+}
