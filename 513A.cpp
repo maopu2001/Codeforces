32ef4fa0921c1077544ffa5e18ee9bb1138deb85
maopu2001 <maopu2001@gmail.com>
2023-09-13 19:47:26 +0600

    13/09/2023

---
+++ 513A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl '\n'
+typedef long long ll;
+const int MOD = 1e9 + 7;
+
+void solve(int T) {
+  int n1, n2, k1, k2;
+  cin >> n1 >> n2 >> k1 >> k2;
+  cout << (n1 > n2 ? "First" : "Second") << nl;
+}
+
+int main() {
+  // cout << setprecision(2) << fixed;
+  // ios_base :: sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int _, i;
+  // cin >> _;
+  // for (i = 1; i <= _; ++i)
+  solve(i);
+  return 0;
+}
